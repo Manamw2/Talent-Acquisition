8e7004fa61dd01173edbbf71b6ac063fb7aafb05
Amina Ramadan <Amina Ramadan@DESKTOP-7H2T0IF>
2025-02-10 15:09:42 +0200

    solve error

--- HrBackOffice/Models/UserViewModel.cs
+++ HrBackOffice/Models/UserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Models;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace HrBackOffice.Models
@@ -11,10 +12,21 @@ namespace HrBackOffice.Models
         public string Email { get; set; }
         public string? EducationLevel { get; set; }
         public string? EnglishLevel { get; set; }
+        public string? Faculty { get; set; }
+        public string? MethodOfContact { get; set; }
+        public DateOnly? BirthDate { get; set; }
+        public string? CvUrl { get; set; }
+
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, ErrorMessage = "The password must be at least {2} characters long.", MinimumLength = 6)]
-        public string Password { get; set; }
-        public IEnumerable<string>? Roles{ get; set; }
+        public string Password { get; set; } = "Temp@1234"; // Default password
+
+        public IEnumerable<string>? Roles { get; set; } = new List<string> { "Applicant" };
+
+        public List<ApplicantExperience> ApplicantExperiences { get; set; } = new();
+        public List<ApplicantSkill> ApplicantSkills { get; set; } = new();
+        public List<ApplicantProject> ApplicantProjects { get; set; } = new();
     }
+
 }

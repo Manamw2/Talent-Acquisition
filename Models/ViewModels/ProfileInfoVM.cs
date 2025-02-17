d27886c6ca8e74be2e435ac4a9d70510f306ecce
Mahmoud Nabil <mahmoud.amr.nabil23@gmail.com>
2025-02-17 05:21:36 +0200

    fix issue in confirm resume info page

--- Models/ViewModels/ProfileInfoVM.cs
+++ Models/ViewModels/ProfileInfoVM.cs
@@ -62,77 +62,17 @@ namespace Models.ViewModels
     {
         public MyInput()
         {
-            // Use standard List initialization
 
-            var Uni = new List<string>
-            {   "Cairo University",
-                "Ain Shams University",
-                "Alexandria University",
-                "Helwan University",
-                "Mansoura University",
-                "Zagazig University",
-                "Assiut University",
-                "Tanta University",
-                "Benha University",
-                "Suez Canal University",
-                "Minia University",
-                "South Valley University",
-                "Fayoum University",
-                "Beni-Suef University",
-                "Sohag University",
-                "Kafr El Sheikh University",
-                "Damietta University",
-                "Port Said University",
-                "Menoufia University",
-                "Al-Azhar University",
-                "The British University in Egypt (BUE)",
-                "The American University in Cairo (AUC)",
-                "German University in Cairo (GUC)",
-                "Misr University for Science and Technology (MUST)",
-                "Future University in Egypt (FUE)",
-                "October 6 University",
-                "Modern Sciences and Arts University (MSA)",
-                "Nahda University",
-                "Sinai University",
-                "Other"
-
-            };
-
-            var Fac = new List<string>
-            {   "Faculty of Engineering",
-                "Faculty of Medicine",
-                "Faculty of Pharmacy",
-                "Faculty of Science",
-                "Faculty of Commerce",
-                "Faculty of Law",
-                "Faculty of Arts",
-                "Faculty of Education",
-                "Faculty of Agriculture",
-                "Faculty of Dentistry",
-                "Faculty of Computer and Artificial Intelligence",
-                "Faculty of Veterinary Medicine",
-                "Faculty of Nursing",
-                "Faculty of Physical Therapy",
-                "Faculty of Tourism and Hotels",
-                "Faculty of Mass Communication",
-                "Faculty of Fine Arts",
-                "Faculty of Applied Arts",
-                "Faculty of Al-Alsun (Languages)",
-                "Faculty of Islamic Studies",
-                "Other"
-            };
             var edus = new List<string> { "Undergraduate", "Graduate" };
             var engs = new List<string> { "Beginner", "Intermediate", "Advanced", "Fluent" };
             var meths = new List<string> { "Email", "Phone" };
 
-            Universities = Uni.Select(u => new SelectListItem { Text = u, Value = u });
-            Faculties = Fac.Select(u => new SelectListItem { Text = u, Value = u });
+
             EducationLevels = edus.Select(u => new SelectListItem { Text = u, Value = u });
             EnglishProficiencyLevels = engs.Select(u => new SelectListItem { Text = u, Value = u });
             MethodOfContactOptions = meths.Select(u => new SelectListItem { Text = u, Value = u });
         }
-        public IEnumerable<SelectListItem> Universities { get; set; }
-        public IEnumerable<SelectListItem> Faculties { get; set; }
+
         public IEnumerable<SelectListItem> EducationLevels { get; set; }
         public IEnumerable<SelectListItem> EnglishProficiencyLevels { get; set; }
         public IEnumerable<SelectListItem> MethodOfContactOptions { get; set; }

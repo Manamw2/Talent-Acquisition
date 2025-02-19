112544e6e26f365531dfde25b1d497825a027677
Amina Ramadan <Amina Ramadan@DESKTOP-7H2T0IF>
2025-02-19 09:47:42 +0200

    phone validation

--- HrBackOffice/Models/UserViewModel.cs
+++ HrBackOffice/Models/UserViewModel.cs
@@ -8,49 +8,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HrBackOffice.Models
 {
-    //public class UserViewModel
-    //{
-    //    public string? Id { get; set; }
-    //    public string UserName { get; set; }
-    //    public string DisplayName { get; set; }
-    //    public string Email { get; set; }
-    //    public string Phone { get; set; } = string.Empty;
-    //    public string EnglishProficiencyLevel { get; set; } = string.Empty;
-    //    public string? EducationLevel { get; set; }
-    //    public string? EnglishLevel { get; set; }
-    //    public string? Faculty { get; set; }
-    //    public string? MethodOfContact { get; set; }
-    //    public DateOnly? BirthDate { get; set; }
-    //    public string? CvUrl { get; set; }
-    //    [Display(Name = "CV File")]
-    //    public IFormFile? CvFile { get; set; } // Add this property for file upload
-
-    //    [DataType(DataType.Password)]
-    //    [Required(ErrorMessage = "Password is required.")]
-    //    [StringLength(100, ErrorMessage = "The password must be at least {2} characters long.", MinimumLength = 6)]
-    //    public string Password { get; set; } = "Temp@1234"; // Default password
-
-    //    public IEnumerable<string>? Roles { get; set; } = new List<string> { "Applicant" };
-
-    //    public List<AppExperienceViewModel> ApplicantExperiences { get; set; } = new();
-    //    public List<AppSkillViewModel> ApplicantSkills { get; set; } = new();
-    //    public List<AppProjectViewModel> ApplicantProjects { get; set; } = new();
-    //    public List<SelectListItem>? EducationLevels { get; set; }
-
-    //    public List<SelectListItem>? EnglishProficiencyLevels { get; set; }
-
-    //    public string? University { get; set; }
-    //    public List<SelectListItem>? Universities { get; set; }
-
-    //    public List<SelectListItem>? Faculties { get; set; }
-    //}
+    
 
     public class UserViewModel
     {
         public string? Id { get; set; }
         public string? UserName { get; set; } = string.Empty;
+        [Display(Name = "Display Name")]
         public string DisplayName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^[0-9]{11}$", ErrorMessage = "Phone number must be exactly 11 digits")]
+        [Display(Name = "Phone Number")]
         public string Phone { get; set; } = string.Empty;
 
         public string? EducationLevel { get; set; }

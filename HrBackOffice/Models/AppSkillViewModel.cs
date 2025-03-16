dcb2d61982c1701324aef01e294bd03ef3ad8b63
Amina Ramadan <Amina Ramadan@DESKTOP-7H2T0IF>
2025-03-16 09:20:03 +0200

    Employees

--- HrBackOffice/Models/AppSkillViewModel.cs
+++ HrBackOffice/Models/AppSkillViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace HrBackOffice.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace HrBackOffice.Models
 {
     public class AppSkillViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Skill name is required.")]
+        [DisplayName("Skill Name")]
+        [StringLength(100, ErrorMessage = "Skill name cannot exceed 100 characters.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Skill name is required.")]
+        [DisplayName("Skill Level")]
         public string Level { get; set; }
     }
 }

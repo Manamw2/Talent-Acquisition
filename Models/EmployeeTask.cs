13181935a4a2cefdc3192a0db929486aa09e0461
Amina Ramadan <Amina Ramadan@DESKTOP-7H2T0IF>
2025-03-17 12:20:38 +0200

    task

--- Models/EmployeeTask.cs
+++ Models/EmployeeTask.cs
@@ -9,11 +9,15 @@ namespace Models
 {
     public class EmployeeTask
     {
+        public int Id { get; set; }  // Primary key
         public int EmployeeId { get; set; }
         [ForeignKey(nameof(EmployeeId))]
         public Employee Employee { get; set; }
         public int TaskId { get; set; }
         [ForeignKey(nameof(TaskId))]
         public HiringTask HiringTask { get; set; }
+
+        public string Status { get; set; } = "New";  // Default status
+        public DateTime AssignedDate { get; set; } = DateTime.Now;
     }
 }
---
+++ Models/ViewModels/HiringTasksViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class HiringTasksViewModel
+    {
+        public List<EmployeeTask> Tasks { get; set; } = new List<EmployeeTask>();
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalTasks { get; set; }
+        public int TotalPages { get; set; }
+        public string? SearchTerm { get; set; }
+        public string? Status { get; set; }
+        public string? Department { get; set; }
+        public string? Batch { get; set; }
+
+        // Filter options for dropdowns
+        public List<string> Statuses { get; set; } = new List<string>();
+        public List<Department> Departments { get; set; } = new List<Department>();
+        public List<Batch> Batches { get; set; } = new List<Batch>();
+    }
+}
